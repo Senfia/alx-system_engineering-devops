f84f57a73eb2d15fb2c3220607448cf15fb9c6b3
Senyo Fiawornu <contactsenyo@gmail.com>
2023-08-29 23:09:53 +0000

    add puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Install and configure an Nginx server with the following requirements:
+
+
+package { 'nginx':
+      ensure => installed,
+}
+
+file_line { 'rewrite redirect':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'server_name _;',
+    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+      content => 'Hello World!',
+      require => Package['nginx'],
+}
+
+service { 'nginx':
+    ensure  => 'running',
+    require => file_line['rewrite redirect'],
+}
